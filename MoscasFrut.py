680147ff8f32fb1a2ab14cd10238fadb3225fbc8
Arturo LVC <arturo.aceves@inaoe.mx>
2024-12-03 20:27:50 -0600

    Modification of prints for the script "MoscasFrut.py"

--- MoscasFrut.py
+++ MoscasFrut.py
@@ -3,13 +3,17 @@ import os
 import time
 import pyautogui
 
+print("Setting up the camera and path, please wait...")
 
-captura = cv2.VideoCapture(1)
-path = r'/home/atrapamocas/Desktop/MoscaFrut/Reporte diario/Trampa 1'
+captura = cv2.VideoCapture(0)
+path = '/run/user/1000/gvfs/google-drive:host=gmail.com,user=lvctfruta/GVfsSharedWithMe/1EKXB6EA8oBaYaB1ZqVub4xDf8eRS2SuV/1Af5moXu0KUXqpgD56vpq8mKAkoOGWBkJ/1dw-MbWP9yfLTxvIsw-GkQBZSCZcquKXW'
+
+#path = '/home/atrapamocas/Desktop'
 start_time = cv2.getTickCount()  # Get the start time
 ti_c =os.path.getmtime(path)
 c_ti = time.ctime(ti_c)
 c_ti2 =c_ti.replace(":", ".")
+
 
 while (captura.isOpened()):
   ret, imagen = captura.read()
@@ -24,7 +28,10 @@ while (captura.isOpened()):
   elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
   if elapsed_time >= 5:  # 2 seconds
       print("2 seconds elapsed, breaking the loop")
-      cv2.imwrite(os.path.join(path, c_ti2 + '.jpg'), imagen)     
+      print("\n With the date: ",c_ti2)
+      print("\n To the path: ", path)
+      x = cv2.imwrite(os.path.join(path, c_ti2 + '.jpg'), imagen)
+      print("Status of cv2.imwrite: ",x)   
       break
 
     
